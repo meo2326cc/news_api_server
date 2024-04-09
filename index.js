5bb5f89f4a86a10d372bc38050c92464f88d0e4e
meo2326cc <cccmsp1@gmail.com>
2024-04-09 19:01:16 +0800

    Enable all CORS requests

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import { config } from "dotenv";
 import { router as news } from './routes/news/news.js'
 import { router as users } from './routes/users/user.js'
 import { router as userdata } from './routes/users/userdata/userdata.js'
+import cors from 'cors'
 const app = Express();
 
-//暫時用不到
+
 app.use(Express.json()) // for parsing application/json
 app.use(Express.text()) // for parsing text
+app.use(cors())
 
 app.use( '/news' , news )
 app.use( '/user' , users )
