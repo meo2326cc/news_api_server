cd201dbe1eacdba14638a465e6743f2074aede91
meo2326cc <cccmsp1@gmail.com>
2024-04-25 17:09:05 +0800

    增加新聞日期資料

--- routes/news/handleData.js
+++ routes/news/handleData.js
@@ -2,15 +2,16 @@ import * as cheerio from 'cheerio';
 
 export  function handleCnaData( data ) {
     const $ = cheerio.load( data )
-    return $('#jsMainList').find('li').map( (item , el )=>{ return {"title":$(el).find('h2').text(), "url": $(el).find('a').attr('href') } } ).toArray()
+    return $('#jsMainList').find('li').map( (item , el )=>{ return {"title":$(el).find('h2').text(), "url": $(el).find('a').attr('href') , "date": $(el).find('.date').text()  } } ).toArray()
 }
 
 export  function handleLtnData( data ) {
     const $ = cheerio.load( data )
-    return $('.list').find('li').map( (item , el )=>{ return {"title":$(el).find('h3').text(), "url": $(el).find('a').attr('href') } } ).toArray()
+    const date = new Date()
+    return $('.list').find('li').map( (item , el )=>{ return {"title":$(el).find('h3').text(), "url": $(el).find('a').attr('href') , "date": `${date.getFullYear()}/${(date.getMonth()+1) < 10 ? ("0" +( date.getMonth()+1) ) : (date.getMonth()+1) }/${date.getDate()} ${$(el).find('.time').text()}` } } ).toArray()
 }
 
 export  function handleCtsData ( data ) {
     const $ = cheerio.load( data )
-    return $('.newslist-container').find('a').map( (item , el )=>{ return {"title":$(el).attr('title'), "url": $(el).attr('href') } } ).toArray()
+    return $('.newslist-container').find('a').map( (item , el )=>{ return {"title":$(el).attr('title'), "url": $(el).attr('href') , "date" : $(el).find('h3 .newstime').text() } } ).toArray()
 }
