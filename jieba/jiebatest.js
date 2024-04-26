b2c5f9e532d7c8db7874363fbf4821ed96469ee5
meo2326cc <cccmsp1@gmail.com>
2024-04-26 23:12:29 +0800

    關鍵字分析功能
    - 使用jieba進行斷詞處理
    -jieba辭典採用 https://github.com/ldkrsi/jieba-zh_TW 專案提供之dict.txt，其來源為中研院斷詞服務之詞庫
    - 依照其 MIT License 使用，本專案僅作為學習用途，不做營利使用，並遵守中研院斷詞服務之服務條款

---
+++ jieba/jiebatest.js
@@ -0,0 +1,53 @@
+import  nodejieba  from "nodejieba";
+
+export default function handleKeywords( newsData ) {
+
+
+  nodejieba.load({
+    userDict: 'jieba/dict.txt',
+  })
+
+
+  const wordCount = {} ;
+
+
+  newsData.forEach((news) => {
+
+    const handleWords = nodejieba.cut(news.title);
+
+    const words = handleWords.filter( word => word.length > 1 )
+
+    words.forEach((word) => {
+
+      let newsType ;
+
+      const dataStructure = { keyWord:'' , totalCount : 1 , cna : 0 , ltn : 0 , cts : 0 }
+
+      if ( news.url.includes('news.cts.com.tw') ) {newsType = 'cts'}
+      if ( news.url.includes('news.ltn.com.tw') ) {newsType = 'ltn'}
+      if ( news.url.includes('news/aipl') ) {newsType = 'cna'}
+      
+
+        if (wordCount[word]) {
+          wordCount[word].totalCount += 1;
+          wordCount[word][newsType] += 1;
+        } else {
+          // init
+          wordCount[word] = dataStructure ;
+          wordCount[word].keyWord = word ;
+          wordCount[word][newsType] = 1;
+        }
+        
+
+    });
+  });
+
+
+  //const sortedWordCount = Object.entries(wordCount).sort((a, b) => b[1] - a[1]);
+  const sortedWordCount = Object.values(wordCount).sort((a , b ) => b.totalCount - a.totalCount  )
+  //log
+  console.log(sortedWordCount.slice(0, 10));
+
+  return sortedWordCount.slice(0,10)
+
+}
--- routes/news/news.js
+++ routes/news/news.js
@@ -2,6 +2,7 @@ import  Express  from "express";
 export const router = Express.Router()
 import { getNewsInit } from "./getNews.js";
 import { verifyUser } from "../../database/database.js";
+import handleKeywords from "../../jieba/jiebatest.js";
 
 const newsInit = getNewsInit()
 
@@ -20,6 +21,25 @@ export async function handleNewsRoute( req , res ) {
 
 }
 
+export async function handleKeyWordsRoute( req , res ) {
+
+  if (!req.headers.authorization) {
+    res.status(401).send("未通過認證");
+  } else if (await verifyUser(req.headers.authorization)) {
+    const route = req.route.path
+    res.set("Content-Type", "application/json"); 
+    //res.status(200).send( await newsInit( route.split("/")[1] ) )
+    const mynews = await newsInit('cna')
+    const allnews = [ ...await newsInit('cna') , ...await newsInit('ltn') , ...await newsInit('cts') ]
+    res.status(200).send( handleKeywords(allnews) )
+
+  } else {
+    res.status(401).send("未通過認證");
+  }
+
+}
+
 router.get("/cna", handleNewsRoute );
 router.get("/ltn", handleNewsRoute );
 router.get("/cts", handleNewsRoute );
+router.get("/keywords" ,handleKeyWordsRoute )
