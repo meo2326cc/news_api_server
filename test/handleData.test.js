17424bfb400e1d44e3d8a6ebc1d824c46b563c9d
meo2326cc <cccmsp1@gmail.com>
2024-05-05 19:49:18 +0800

    api功能測試

--- test/handleData.test.js
+++ test/handleData.test.js
@@ -1,35 +1,21 @@
-import  { getCnaNews , getLtnNews } from '../routes/news/getNews.js'
+import  { getNewsInit } from '../routes/news/getNews.js'
 
-// test('cna_data_content', async() => {
-//      expect(getCnaNews()).resolves.toHaveReturned();
-//   });
+ test.each(['cna' , 'cts' , 'pts' , 'keywords' ])
+ ( '模擬隨機api路徑取得資料' , async( path )=>{
+     
+    //建立環境
+    const newsInit = await getNewsInit()
+    let result;
 
-// test('ltn_data_content', () => {
-//      expect(getLtnNews()).resolves.toHaveReturned();
-//   });
+      try {
+        result = await newsInit( path ); 
+        console.log( path , result )
+        // 結果
+        expect(result).toBeInstanceOf(Array);
 
-// describe('fetchRemoteData', () => {
-//    it('should return an array when successful', async () => {
-//      const result = await getCnaNews(); // Assuming fetchRemoteData is an async function
-//      expect(result).toBeInstanceOf(Array);
-//    });
- 
-//    it('should return a string when failed', async () => {
-//      // Assuming you have a way to simulate failure, for example using a mock or stub
-//      // If fetchRemoteData is not an async function, handle error synchronously
-//      const result = await getCnaNews(); // Assuming fetchRemoteData is an async function
-//      // Simulate failure and return string
-//      expect(result).toEqual(expect.any(String));
-//    });
-//  });
-
-it('should return an array when successful', async () => {
-   let result;
-   try {
-     result = await getCnaNews(); // Assuming fetchRemoteData is an async function
-   } catch (error) {
-     // If an error is caught, fail the test
-     fail('fetchRemoteData should not throw an error');
-   }
-   expect(result).toBeInstanceOf(Array);
- });
+      } catch (error) {
+        
+          expect(typeof error).toBe('object')
+      }
+     
+ } )
